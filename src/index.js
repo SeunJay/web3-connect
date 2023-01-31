6ac8aa800f2dbeb086c1156c4186525abd8d5f1d
seunjay <seunjay92@gmail.com>
2023-01-31 13:06:42 +0100

    set up web3 provider

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Web3ReactProvider } from "@web3-react/core";
+import { ethers } from "ethers";
 
-import "./index.css";
 import App from "./App";
+
+const getLibrary = (provider) => {
+  const library = new ethers.providers.Web3Provider(provider);
+  library.pollingInterval = 8000; // frequency provider is polling
+  return library;
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <Web3ReactProvider getLibrary={getLibrary}>
+        <App />
+      </Web3ReactProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
